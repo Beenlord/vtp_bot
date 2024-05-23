8acda94632947d061bff3f2f08304df20411c089
Vovikkko <45092587+Beenlord@users.noreply.github.com>
2024-05-23 17:34:50 +0300

    Wip

--- modules/BotEvents.js
+++ modules/BotEvents.js
@@ -27,24 +27,65 @@ export default class BotEvents extends BotCommands
 		this.channel = channel_id;
 	}
 
-	static sendMessage(msg, text) {
-		this.bot.sendMessage(msg?.from?.id, text);
-	}
-
-	static sendPost(text, vars = null) {
-		let value = text;
-		if (vars) value = Handlebars.compile(text)(vars);
-		this.channel && this.bot.sendMessage(this.channel, value, {
+	/**
+	 * Метод для отправки сообщений по нужному id
+	 * 
+	 * @param {*} id ID Сообщения или объект telegram api
+	 * @param {*} msg Текст сообщения (можно в формате html)
+	 */
+	static sendMessage(id, msg) {
+		id && this.bot.sendMessage(id?.from?.id ?? id, msg, {
 			parse_mode: 'HTML',
 		});
 	}
 
-	static sendTemplatePost(tpl_name, vars = null) {
-		tpl_name = `${tpl_name}.htm`;
-		const path_to_file = path.join('tpls', tpl_name);
+	/**
+	 * Обёртка над sendMessage для
+	 * отпарвки соообщений по шаблону
+	 * 
+	 * @param {number|object} id ID Сообщения или объект telegram api
+	 * @param {string} tpl Имя шаблона из папки tpls
+	 * @param {object} vars Переменные для шаблона Handlebars
+	 */
+	static sendTemplateMessage(id, tpl, vars = {}) {
+		tpl = getTemplate(tpl);
+		tpl = Handlebars.compile(tpl)(vars);
+		this.sendMessage(id, tpl);
+	}
+
+	/**
+	 * Отправка сообщений в канал
+	 * 
+	 * @param {string} msg Текст сообщения (можно в формате html)
+	 */
+	static sendPost(msg) {
+		this.channel && this.sendMessage(this.channel, msg);
+	}
+
+	/**
+	 * Обёрка над sendTemplateMessage для
+	 * отправки сообщений в канал по шаблону
+	 * 
+	 * @param {string} tpl Имя шаблона из папки tpls
+	 * @param {object} vars Переменные для шаблона Handlebars
+	 */
+	static sendTemplatePost(tpl, vars = {}) {
+		this.channel && this.sendTemplateMessage(this.channel, tpl, vars);
+	}
+
+	/**
+	 * Метод получения шаблона из папки
+	 * tpls по названию
+	 * 
+	 * @param {string} tpl Имя шаблона из папки tpls
+	 * @returns Содержимое файла шаблона
+	 */
+	getTemplate(tpl) {
+		tpl = `${tpl}.htm`;
+		const path_to_file = path.join('tpls', tpl);
 		if (fs.existsSync(path_to_file)) {
-			const file_value = fs.readFileSync(path_to_file, 'utf8');
-			this.sendPost(file_value, vars);
+			return fs.readFileSync(path_to_file, 'utf8');
 		}
+		return '';
 	}
 }
