7a91749c835a3a837590846c9ba6363783fe4d69
Beenlord <tombeenlord@gmail.com>
2024-06-29 03:02:52 +0300

    Fix

--- core/VtpTelegramBot.ts
+++ core/VtpTelegramBot.ts
@@ -2,7 +2,7 @@ import TelegramBot from '@module/TelegramBot.ts';
 
 export default class VtpTelegramBot extends TelegramBot {
 	constructor() {
-		super(Deno.env.get('BOT_TOKEN'));
+		super(Deno.env.get('TOKEN'));
 		console.log('Launch - successful!');
 	}
 }
